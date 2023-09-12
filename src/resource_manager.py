86f6c45d6c1289c7f0e89d26698230d43a8c5b9e
Shaker Mahmud Khandaker <skhandaker@fbk.eu>
2023-09-12 15:01:24 +0200

    update resource manager;

--- src/resource_manager.py
+++ src/resource_manager.py
@@ -31,20 +31,47 @@ class ResourceManager:
                 methods.extend(entry.get('methods', []))
         return methods
     
+    def get_details_by_project_class_and_method(self, project_name, class_name, method_name):
+        method_details = []
+        for entry in self.data:
+            if (
+                entry.get('project_name') == project_name
+                and entry.get('class_name') == class_name
+            ):
+                class_methods = entry.get('methods', [])
+                for method in class_methods:
+                    if method.get('method_name') == method_name:
+                        method_details.append(method)
+        return method_details
+
+    
 # Example usage:
 if __name__ == '__main__':
     json_file_path = db_file  # Replace with the actual file path
     manager = ResourceManager(json_file_path)
 
     project_name_to_query = 'lang_1_buggy'  # Replace with the project name you want to query
-    # entries_with_contains_test = manager.get_entries_with_contains_test(project_name_to_query)
-    # entries_without_contains_test = manager.get_entries_without_contains_test(project_name_to_query)
     
-
     class_name_to_query = 'NumberUtils_ESTest'
+    method_name_to_query = 'test111'
 
-    methods_for_project_and_class = manager.get_methods_by_project_and_class(project_name_to_query, class_name_to_query)
 
-    print("Methods for Project and Class:")
-    for method in methods_for_project_and_class:
-        print(method.get('method_name'), method.get("parameters"))
+    class_name_to_query = 'NumberUtils'
+    method_name_to_query = 'min'
+
+    # print("Methods for Project and Class:")
+    # for method in methods_for_project_and_class:
+    #     print(method.get('method_name'), method.get("parameters"))
+
+    method_details = manager.get_details_by_project_class_and_method(
+        project_name_to_query, class_name_to_query, method_name_to_query
+    )
+
+    if method_details:
+        print("Method Details:")
+        
+        for method in method_details:
+            for key, value in method.items():
+                print(f"{key}: {value}")
+    else:
+        print(f"No details found for method '{method_name_to_query}' in class '{class_name_to_query}' of project '{project_name_to_query}'.")
